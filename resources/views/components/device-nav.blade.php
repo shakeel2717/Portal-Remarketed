8ef2ea94aad1628576793b6002b26f65ae982174
shakeel2717 <shakeel2717@gmail.com>
2021-08-24 15:13:41 -0700

    Dashboard Show All and Other Seprate Category Displayed

--- resources/views/components/device-nav.blade.php
+++ resources/views/components/device-nav.blade.php
@@ -2,13 +2,16 @@
 <div class="text-center">
     <ul class="nav nav-segment nav-pills mb-7" role="tablist">
         <li class="nav-item">
-            <a class="nav-link active" href="{{ route('devicesWorking') }}">Working</a>
+            <a class="nav-link {{ (Illuminate\Support\Facades\Route::currentRouteName() == "dashboard") ? "active" : "" }}" href="{{ route('dashboard') }}">All</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('devicesRefurbishing') }}">Refurbishing</a>
+            <a class="nav-link {{ (Illuminate\Support\Facades\Route::currentRouteName() == "devicesWorking") ? "active" : "" }}" href="{{ route('devicesWorking') }}">Working</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('devicesmotherboard') }}">Refurbishing</a>
+            <a class="nav-link {{ (Illuminate\Support\Facades\Route::currentRouteName() == "devicesRefurbishing") ? "active" : "" }}" href="{{ route('devicesRefurbishing') }}">Refurbishing</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ (Illuminate\Support\Facades\Route::currentRouteName() == "devicesmotherboard") ? "active" : "" }}" href="{{ route('devicesmotherboard') }}">Refurbishing</a>
         </li>
     </ul>
 </div>
