2ea1ca6d0f8488b0ede81038c390fa0b99677140
shakeel2717 <shakeel2717@gmail.com>
2021-09-15 11:41:51 -0700

    Order Nav Code tranfser into view form compoennt

--- app/View/Components/ordersNav.php
+++ app/View/Components/ordersNav.php
@@ -2,15 +2,10 @@
 
 namespace App\View\Components;
 
-use App\Models\order;
 use Illuminate\View\Component;
 
 class ordersNav extends Component
 {
-    // public $draft;
-    // public $quote;
-    // public $reserved;
-    // public $shipped;
     /**
      * Create a new component instance.
      *
@@ -18,6 +13,7 @@ class ordersNav extends Component
      */
     public function __construct()
     {
+        //
     }
 
     /**
@@ -27,15 +23,6 @@ class ordersNav extends Component
      */
     public function render()
     {
-        $draft = order::where('status', 'Draft')->where('users_id', session('user')[0]->id)->count();
-        $quote = order::where('status', 'Quote')->where('users_id', session('user')[0]->id)->count();
-        $reserved = order::where('status', 'Reserved')->where('users_id', session('user')[0]->id)->count();
-        $shipped = order::where('status', 'Shipped')->where('users_id', session('user')[0]->id)->count();
-        return view('components.orders-nav', [
-            'draft' => $draft,
-            'quote' => $quote,
-            'reserved' => $reserved,
-            'shipped' => $shipped,
-        ]);
+        return view('components.orders-nav');
     }
 }
--- resources/views/components/orders-nav.blade.php
+++ resources/views/components/orders-nav.blade.php
@@ -3,19 +3,19 @@
     <ul class="nav nav-segment nav-pills mb-7" role="tablist">
         <li class="nav-item">
             <a class="nav-link {{ Illuminate\Support\Facades\Route::currentRouteName() == 'draftsOrders' ? 'active' : '' }}"
-                href="{{ route('draftsOrders') }}">Drafts <span class="badge badge-primary">{{$draft}}</span></a>
+                href="{{ route('draftsOrders') }}">Drafts <span class="badge badge-primary">{{ DB::table('orders')->where('status', 'Draft')->where('users_id', session('user')[0]->id)->count();}}</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Illuminate\Support\Facades\Route::currentRouteName() == 'quoteOrders' ? 'active' : '' }}"
-                href="{{ route('quoteOrders') }}">Under Review <span class="badge badge-primary">{{$quote}}</span></a>
+                href="{{ route('quoteOrders') }}">Under Review <span class="badge badge-primary">{{DB::table('orders')->where('status', 'Quote')->where('users_id', session('user')[0]->id)->count();}}</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Illuminate\Support\Facades\Route::currentRouteName() == 'reservedOrders' ? 'active' : '' }}"
-                href="{{ route('reservedOrders') }}">Open Offers <span class="badge badge-primary">{{$reserved}}</span></a>
+                href="{{ route('reservedOrders') }}">Open Offers <span class="badge badge-primary">{{DB::table('orders')->where('status', 'Reserved')->where('users_id', session('user')[0]->id)->count();}}</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Illuminate\Support\Facades\Route::currentRouteName() == 'invoicedOrders' ? 'active' : '' }}"
-                href="{{ route('invoicedOrders') }}">Shipped <span class="badge badge-primary">{{$shipped}}</span></a>
+                href="{{ route('invoicedOrders') }}">Shipped <span class="badge badge-primary">{{DB::table('orders')->where('status', 'Shipped')->where('users_id', session('user')[0]->id)->count();}}</span></a>
         </li>
     </ul>
 </div>
